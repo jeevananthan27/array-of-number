4e6bd0284e772eaa98d49bc4e794874f73260711
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2023-03-13 21:31:21 +0530

    Create array-of-number.c

---
+++ array-of-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int len;
+    printf ("Enter the value: ");
+    scanf ("%d",&len);
+    int arr[len];
+    int sum=1;
+    for (int i=0; i<len; i++);
+    for (int j=0; j<len; j++)
+        {
+            printf("%d ",j);
+   
+        }
+        return 0;
+        }
